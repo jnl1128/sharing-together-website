6bb88dd5227a8a86c30afce90a80804be66ebd15
Narin <jnl1128@naver.com>
2020-05-29 17:25:52 +0900

    views.py
    to_gather 페이지에 필요한 view 수정했습니다.

--- website/views.py
+++ website/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from django.views import View
+from .forms import PostForm
 
 # Create your views here.
 
-def post_list(request):
+def to_gather(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'website/post_list.html', {'posts': posts})
+    return render(request, 'website/to_gather.html', {'posts': posts})
 
-def post_detail(request, pk):
+def to_gather_list(request, pk):
     post = get_object_or_404(Post, pk = pk)
-    return render(request, 'website/post_detail.html', {'post': post})
+    return render(request, 'website/to_gather_list.html', {'post': post})
+
+def to_gather_list_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('to_gather_list', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'website/to_gather_list_edit.html', {'form': form})
+
+def to_gather_list_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('to_gather_list', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'website/to_gather_list_edit.html', {'form': form})
