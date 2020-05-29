ffd984d4013e45861ef1de4461e37f4d99c90664
Jia0507 <yuun0289@naver.com>
2020-05-30 07:25:45 +0900

    main.html 생성해서 우선 텍스트만 넣었습니다. main.css가 적용이 안돼서 지금 고민이네요 ..

--- website/views.py
+++ website/views.py
@@ -12,3 +12,6 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'website/post_detail.html', {'post': post})
+
+def main (request):
+    return render(request, 'website/main.html', {})
