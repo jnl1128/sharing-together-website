a6d976764afa297add4b961dec9953a76c7fed4a
Narin <jnl1128@naver.com>
2020-05-29 17:23:32 +0900

    urls.py
    to_gather 페이지에 필요한 url 수정했습니다.

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name = 'post_list'),
-    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
-
+    path('to_gather/', views.to_gather, name = 'to_gather'),
+    path('to_gather/<int:pk>/', views.to_gather_list, name = 'to_gather_list'),
+    path('to_gather/new/', views.to_gather_list_new, name='to_gather_list_new'),
+    path('to_gather/<int:pk>/edit/', views.to_gather_list_edit, name='to_gather_list_edit'),
 ]
