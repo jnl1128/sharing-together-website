db455442ccc483e4aeeceabca8d9ceedcf7431fd
Narin <jnl1128@naver.com>
2020-05-30 17:00:25 +0900

    admin.py
    새로운 Chat 모델 추가했습니다.

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
-
+from .models import Post, Chat
+# to_gather 모델
 admin.site.register(Post)
+
+# be_together 모델
+admin.site.register(Chat)
