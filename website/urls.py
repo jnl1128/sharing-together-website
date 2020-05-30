bdd18cbd020552944eff6bc0f5b8b26be7cae30d
Narin <jnl1128@naver.com>
2020-05-30 17:01:21 +0900

    urls.py
    be_togethe관련된 url 추가햇습니다.

--- website/urls.py
+++ website/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('to_gather/<int:pk>/', views.to_gather_list, name = 'to_gather_list'),
     path('to_gather/new/', views.to_gather_list_new, name='to_gather_list_new'),
     path('to_gather/<int:pk>/edit/', views.to_gather_list_edit, name='to_gather_list_edit'),
+    path('be_together/', views.be_together, name = 'be_together'),
+    path('be_together/<int:pk>/', views.be_together_list, name = 'be_together_list'),
+
 ]
