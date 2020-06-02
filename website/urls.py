648a9771db92aa30e600630ede71e19b1925faa1
Narin <jnl1128@naver.com>
2020-06-02 16:26:09 +0900

    urls.py
    1. 글 새로 작성하게끔 to_gather_new 뷰 url이랑
    2. 글 수정할 수 있게끔 to_gather_edit 뷰 url 만들었는데 +모양이 content_list_block에 안떠서 다시 수정해야 할거 같아요
    
    3. 지아님이 만드신 main.html url도 추가했습니다.

--- website/urls.py
+++ website/urls.py
@@ -7,9 +7,11 @@ from . import views
 urlpatterns = [
     path('to_gather/', views.to_gather, name = 'to_gather'),
     path('to_gather/<int:pk>/', views.to_gather_list, name = 'to_gather_list'),
-    path('to_gather/new/', views.to_gather_list_new, name='to_gather_list_new'),
-    path('to_gather/<int:pk>/edit/', views.to_gather_list_edit, name='to_gather_list_edit'),
+    path('to_gather/new/', views.to_gather_new, name = 'to_gather_new'), # 새로운 글 쓰기 위함
+    path('to_gather/<int:pk>/edit/', views.to_gather_edit, name = 'to_gather_edit'), # 글 수정 위함
     path('be_together/', views.be_together, name = 'be_together'),
     path('be_together/<int:pk>/', views.be_together_list, name = 'be_together_list'),
+    path('main.html/', views.main, name = 'main'),
+
 
 ]
