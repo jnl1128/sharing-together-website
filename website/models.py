e7b49dbd2b4998a7ae6e7a52cb4c1e1bc0edcf8d
Narin <jnl1128@naver.com>
2020-06-13 14:56:57 +0900

    no message

--- website/models.py
+++ website/models.py
@@ -28,7 +28,7 @@ class Chat(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, null = True)
     published_date = models.DateTimeField(
             blank=True, null=True)
 
