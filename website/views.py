7156570e2f8e6038f239a44101d65aaa63482b92
Narin <jnl1128@naver.com>
2020-06-13 14:58:28 +0900

    views.py
    be_together 새로운 글쓰기랑 수정하기 위한 view 추가했습니다.

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Chat
 from django.views import View
-from .forms import PostForm
+from .forms import PostForm, ChatForm
 
 # Create your views here.
 
@@ -37,15 +37,18 @@ def to_gather_edit(request, pk):
     if request.method == "POST":
         form = PostForm(instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
+            post = form.save(commit = False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('to_gather_list', pk=post.pk) 
+            return redirect('to_gather_list', pk=post.pk)
 
     else:
         form = PostForm(instance=post)
     return render(request, 'website/to_gather_edit.html', {'form': form})  # 글 수정하는 view
+
+
+
 
 #be_together view 관련
 def be_together(request):
@@ -55,3 +58,33 @@ def be_together(request):
 def be_together_list(request, pk):
     chat = get_object_or_404(Chat, pk = pk)
     return render(request, 'website/be_together_list.html', {'chat': chat})
+
+
+
+def be_together_new(request):
+    if request.method == "POST":
+        form = ChatForm(request.POST)
+        if form.is_valid():    ## 잘못된 값이 저장되는 것을 방지하기 위해
+            chat = form.save(commit = False)
+            chat.author = request.user
+            chat.published_date = timezone.now()
+            chat.save()
+            return redirect('be_together_list', pk=chat.pk)
+    else:
+        form = ChatForm()
+    return render(request, 'website/be_together_edit.html', {'form': form}) # 새로운 글 만드는 view
+
+def be_together_edit(request, pk):
+    chat = get_object_or_404(Chat, pk = pk)
+    if request.method == "POST":
+        form = ChatForm(instance = chat)
+        if form.is_valid():
+            chat = form.save(commit=False)
+            chat.author = request.user
+            chat.published_date = timezone.now()
+            chat.save()
+            return redirect('be_together_list', pk=chat.pk)
+
+    else:
+        form = ChatForm(instance=chat)
+    return render(request, 'website/be_together_edit.html', {'form': form})  # 글 수정하는 view
