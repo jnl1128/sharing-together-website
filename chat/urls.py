2e5bc15f0765bb209c491317297183682bac985f
Choalter <eodks989@gmail.com>
2020-06-13 16:14:42 +0900

    account: 회원 관리 앱 입니다. login, signup 페이지로 이동하기 위해서 base.html 의 button 태그를 a 태그로 수정했습니다.

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.be_together, name='be_together'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'channels',
     'chat',
     'django.contrib.admin',
