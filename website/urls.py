313a123d5ec755039d25cd1901d62337e9f1bbe8
Jia0507 <yuun0289@naver.com>
2020-06-20 17:57:22 +0900

    최종입니다

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('to_gather/<int:pk>/', views.to_gather_list, name='to_gather_list'),
     path('to_gather/new/', views.to_gather_new, name='to_gather_new'),  # 새로운 글 쓰기 위함
     path('to_gather/<int:pk>/edit/', views.to_gather_edit, name='to_gather_edit'),  # 글 수정 위함
+    path('come_together/', views.come_together, name='come_together'),
+
 ]
